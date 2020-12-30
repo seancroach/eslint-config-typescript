fcce8bea12f8b9725af72f9efc7c4e146eb77a81
seancroach <me@seancroach.dev>
2020-12-30 13:58:36 -0800

    style: tabs > spaces

--- test/index.js
+++ test/index.js
@@ -2,12 +2,12 @@ const test = require("ava");
 const { CLIEngine } = require("eslint");
 
 test("validate rule syntax", (t) => {
-  const linter = new CLIEngine({
-    configFile: "index.js",
-    useEslintrc: false,
-  });
+	const linter = new CLIEngine({
+		configFile: "index.js",
+		useEslintrc: false,
+	});
 
-  const report = linter.executeOnFiles("test/testFile.ts");
+	const report = linter.executeOnFiles("test/testFile.ts");
 
-  t.is(report.errorCount, 0, JSON.stringify(report.results[0].messages));
+	t.is(report.errorCount, 0, JSON.stringify(report.results[0].messages));
 });
